db0e0a9eb6e0cae806e68524790e262f073ffd27
0xdeadcell <matt2wise@gmail.com>
2020-08-21 18:41:01 -0500

    Link added to some katas

--- kata7/BAD HASH - String to Int.py
+++ kata7/BAD HASH - String to Int.py
@@ -3,3 +3,4 @@ def string_hash(s):
     ascii_difference = sum([ ord(s[j + 1]) - ord(i) for j, i in enumerate(s[:-1])])
     shifted = (ascii_sum | ascii_difference) & (( ~ascii_sum) << 2)
     return shifted ^ (32 * (s.count(" ") + 1))
+# https://www.codewars.com/kata/596d93bd9b6a5df4de000049/train/python
